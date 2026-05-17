fe3469f2bd117c2b0b1a16089d47d2b75f81b14f
0liveira-victor <jvti.profissional@gmail.com>
2026-05-17 20:44:04 -0300

    feat: add script to normalize app icon to 1024x1024 PNG format

---
+++ tool/prepare_launcher_icon.dart
@@ -0,0 +1,25 @@
+import 'dart:io';
+
+import 'package:image/image.dart' as img;
+
+void main() {
+  const iconPath = 'assets/icon/app_icon.png';
+
+  final iconFile = File(iconPath);
+  final iconBytes = iconFile.readAsBytesSync();
+  final decodedImage = img.decodeImage(iconBytes);
+
+  if (decodedImage == null) {
+    throw StateError('Could not decode $iconPath.');
+  }
+
+  final normalizedImage = img.copyResize(
+    decodedImage,
+    width: 1024,
+    height: 1024,
+    interpolation: img.Interpolation.cubic,
+  );
+
+  iconFile.writeAsBytesSync(img.encodePng(normalizedImage));
+  stdout.writeln('Normalized $iconPath to a 1024x1024 PNG.');
+}
